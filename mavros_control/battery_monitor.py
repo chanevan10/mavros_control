1a7250943d4c99c3876d8acab035a73c0c18eecb
Evan <157781542+chanevan10@users.noreply.github.com>
2025-08-13 17:13:01 -0500

    Interpolated voltage based on current, kept voltage threshold

--- mavros_control/battery_monitor.py
+++ mavros_control/battery_monitor.py
@@ -19,9 +19,10 @@ class BatteryMonitorNode(Node):
         self.declare_parameter('voltage_threshold', 14.8, description)  # Default threshold, can be changed via ROS parameter
         self.get_logger().info(f"Voltage threshold set to {self.get_parameter('voltage_threshold').get_parameter_value().double_value}")
 
-        description = ParameterDescriptor(description="Current threshold for battery warning")
-        self.declare_parameter('current_threshold', 0.8, description)  # Default current threshold, can be changed via ROS parameter
-        self.get_logger().info(f"Current threshold set to {self.get_parameter('current_threshold').get_parameter_value().double_value}")
+        # no need for current threshold
+        # description = ParameterDescriptor(description="Current threshold for battery warning")
+        # self.declare_parameter('current_threshold', 0.8, description)  # Default current threshold, can be changed via ROS parameter
+        # self.get_logger().info(f"Current threshold set to {self.get_parameter('current_threshold').get_parameter_value().double_value}")
 
         self.volt_subscriber = self.create_subscription(BatteryState, '/mavros/battery', self.battery_callback, SENSOR_QOS)
         self.timer = self.create_timer(30, self.reading_callback)
@@ -35,13 +36,13 @@ class BatteryMonitorNode(Node):
 
         # TODO: in the future check discharge curve for different current draws
         # TODO: use discharge graph to take in voltage and current and get what the voltage would be at 0A
-
-        voltage_threshold = (-0.0285*(abs(self.current)) + 3.73) * 4
-
-        if self.volts < voltage_threshold:
-            self.get_logger().warn(f"Voltage below threshold at {self.current} A: {self.volts} V < {voltage_threshold} V")
+        voltage_threshold = self.get_parameter('voltage_threshold').get_parameter_value().double_value
+        interpolated_volts = self.volts + 0.01924 * abs(self.current)
+        
+        if interpolated_volts < voltage_threshold:
+            self.get_logger().warn(f"WARNING: BATTERY CAPACITY LOW: {self.voltage} V is too low at {self.current} A")
         else:
-            self.get_logger().info(f"Voltage nominal: {self.volts}V at {abs(self.current)}A with voltage threshold {voltage_threshold} V")
+            self.get_logger().info(f"Voltage nominal, {self.voltage} is {interpolated_volts} at 0 A")
 
         
 
