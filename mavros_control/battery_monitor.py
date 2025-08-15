75d0f4bc301546ce0b36f18155d318fe7b9b1a04
chanevan10 <chanevan10@gmail.com>
2025-08-15 12:43:25 -0500

    updated the code to use linear regression to check battery status

--- mavros_control/battery_monitor.py
+++ mavros_control/battery_monitor.py
@@ -1,4 +1,5 @@
 import rclpy
+from scipy import stats
 from rclpy.node import Node
 from rcl_interfaces.msg import ParameterDescriptor
 
@@ -9,6 +10,7 @@ class BatteryMonitorNode(Node):
         super().__init__('battery_monitor_node')
         self.volts = 0.0
         self.volt_buffer = []
+        self.current_buffer = []
         self.current = 0.0
 
         SENSOR_QOS = rclpy.qos.qos_profile_sensor_data
@@ -19,30 +21,37 @@ class BatteryMonitorNode(Node):
         self.declare_parameter('voltage_threshold', 14.8, description)  # Default threshold, can be changed via ROS parameter
         self.get_logger().info(f"Voltage threshold set to {self.get_parameter('voltage_threshold').get_parameter_value().double_value}")
 
-        # no need for current threshold
-        # description = ParameterDescriptor(description="Current threshold for battery warning")
-        # self.declare_parameter('current_threshold', 0.8, description)  # Default current threshold, can be changed via ROS parameter
-        # self.get_logger().info(f"Current threshold set to {self.get_parameter('current_threshold').get_parameter_value().double_value}")
-
         self.volt_subscriber = self.create_subscription(BatteryState, '/mavros/battery', self.battery_callback, SENSOR_QOS)
-        self.timer = self.create_timer(30, self.reading_callback)
+        self.reading_timer = self.create_timer(0.1, self.reading_callback)
+        
+        self.delay_timer = self.create_timer(30, self.start_delay_timer)
+        self.calc_timer = self.create_timer(10, self.check_battery_status)
+
+    def start_delay_timer(self):
+        self.get_logger().info("Starting battery status checks after 30 seconds delay.")
+        self.delay_timer.cancel()
     
     def battery_callback(self, msg: BatteryState):
         # get necessary information from battery topic
         self.volts = msg.voltage
         self.current = msg.current
 
+    # Puts voltage and current readings into buffers every 0.1 seconds
     def reading_callback(self):
+        if len(self.volt_buffer) and len(self.current_buffer) > 300:
+            self.volt_buffer.pop(0)
+            self.current_buffer.pop(0)
+        self.volt_buffer.append(self.volts)
+        self.current_buffer.append(self.current)
 
-        # TODO: V/A is not a linear relationship, at higher current draws, this measure is more inaccurate
-        # TODO: use discharge graph to take in voltage and current and get what the voltage would be at 0A
-        voltage_threshold = self.get_parameter('voltage_threshold').get_parameter_value().double_value
-        interpolated_volts = self.volts + (4 * 0.01924 * abs(self.current))
-        
-        if interpolated_volts < voltage_threshold:
-            self.get_logger().warn(f"WARNING: BATTERY CAPACITY LOW: {self.voltage} V is too low at {self.current} A")
-        else:
-            self.get_logger().info(f"Voltage nominal, {self.voltage} is {interpolated_volts} at 0 A")
+    # Checks battery status via linear regression
+    def check_battery_status(self):
+        slope, intercept, r, p, std_err = stats.linregress(self.current_buffer, self.volt_buffer)
+        self.get_logger().info(f"Calculated 0A Voltage: {intercept}, Calculated Volts/Current Slope: {slope}")
+        if intercept < self.get_parameter('voltage_threshold').get_parameter_value().double_value:
+            self.get_logger().warn(f"Battery voltage is below threshold: {intercept} < {self.get_parameter('voltage_threshold').get_parameter_value().double_value}")
+
+
 
         
 
@@ -53,4 +62,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
